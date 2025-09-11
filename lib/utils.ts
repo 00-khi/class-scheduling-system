68c352a7e9ff79fc16d85ce68d9c01582ed12d92
khiancarasicas <khianreendy.c@gmail.com>
2025-09-11 14:48:42 +0800

    refactor: utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,19 +1,7 @@
-// lib/utils.ts
-
-/**
- * Converts a string to uppercase.
- * @param str The string to convert.
- * @returns The uppercase string.
- */
 export function toUppercase(str: string): string {
   return removeExtraSpaces(str.toUpperCase());
 }
 
-/**
- * Capitalizes the first letter of each word in a string.
- * @param str The string to capitalize.
- * @returns The capitalized string.
- */
 export function capitalizeEachWord(str: string): string {
   if (!str) return "";
   return removeExtraSpaces(
