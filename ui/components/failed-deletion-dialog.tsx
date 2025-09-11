81a00b05a70d8ec82bece07e75a9f20c08c7c617
khiancarasicas <khianreendy.c@gmail.com>
2025-09-11 22:31:13 +0800

    add: failed deletion dialog component

---
+++ ui/components/failed-deletion-dialog.tsx
@@ -0,0 +1,47 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "../shadcn/alert-dialog";
+
+export function FailedDeletionDialog({
+  open,
+  onClose,
+  failedCount,
+  failedReasons,
+}: {
+  open: boolean;
+  onClose: () => void;
+  failedCount: number;
+  failedReasons: string[];
+}) {
+  return (
+    <AlertDialog open={open} onOpenChange={onClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Failed to delete {failedCount} item(s)
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Some deletions could not be completed. Reasons:
+          </AlertDialogDescription>
+          <AlertDialogDescription className="text-destructive">
+            <ul>
+              {failedReasons.map((error, idx) => (
+                <li key={idx}>{`- ${error}`}</li>
+              ))}
+            </ul>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction onClick={onClose}>Close</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
