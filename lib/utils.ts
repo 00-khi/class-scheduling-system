a4d865caa67264298e3e09dcc9dbe06f925eaa80
khiancarasicas <khianreendy.c@gmail.com>
2025-09-15 14:27:55 +0800

    utils added splitCamelCaseAndNumbers()

--- lib/utils.ts
+++ lib/utils.ts
@@ -24,3 +24,11 @@ export function removeExtraSpaces(str: string): string {
   if (!str) return "";
   return str.trim().replace(/\s+/g, " ");
 }
+
+export function splitCamelCaseAndNumbers(str: string): string {
+  if (!str) return "";
+  const spaced = str
+    .replace(/([a-z])([A-Z0-9])/g, "$1 $2")
+    .replace(/([0-9])([A-Za-z])/g, "$1 $2");
+  return capitalizeEachWord(spaced);
+}
