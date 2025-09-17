f14350222127c4fa403c3eca453143676895df06
khiancarasicas <khianreendy.c@gmail.com>
2025-09-17 19:43:47 +0800

    added api for getting subjects from section

---
+++ app/api/sections/[id]/subjects/route.ts
@@ -0,0 +1,21 @@
+import { createApiHandler } from "@/lib/api/api-handler";
+import { createEntityHandlers } from "@/lib/api/entity-handler";
+import { prisma } from "@/lib/prisma";
+import { Section } from "@prisma/client";
+
+const handlers = createEntityHandlers<Section>({
+  model: "section",
+  include: {
+    course: true,
+  },
+  formatResponse: async (section) => {
+    const courseSubjects = await prisma.courseSubject.findMany({
+      where: { courseId: section.courseId, year: section.year },
+      include: { subject: true },
+    });
+
+    return courseSubjects.map((cs) => cs.subject);
+  },
+});
+
+export const GET = createApiHandler(handlers.GET);
