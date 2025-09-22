5a10dc2698bce6727f915dbbbb2c3034e63984ab
khiancarasicas <khianreendy.c@gmail.com>
2025-09-22 23:56:11 +0800

    added computation of scheduled minutes for each subject based on start and end time

--- app/api/sections/[id]/subjects/route.ts
+++ app/api/sections/[id]/subjects/route.ts
@@ -1,6 +1,7 @@
 import { createApiHandler } from "@/lib/api/api-handler";
 import { createEntityHandlers } from "@/lib/api/entity-handler";
 import { prisma } from "@/lib/prisma";
+import { diffMinutes } from "@/lib/scheduleUtils";
 import { Section } from "@prisma/client";
 
 const handlers = createEntityHandlers<Section>({
@@ -22,7 +23,24 @@ const handlers = createEntityHandlers<Section>({
       },
     });
 
-    return courseSubjects.map((cs) => cs.subject);
+    return courseSubjects
+      .map((cs) => {
+        const subject = cs.subject;
+
+        const scheduledMinutes = subject.scheduledSubject.reduce(
+          (sum, sched) => sum + diffMinutes(sched.startTime, sched.endTime),
+          0
+        );
+
+        const requiredMinutes = subject.units * 60;
+
+        return {
+          ...subject,
+          scheduledMinutes,
+          requiredMinutes,
+        };
+      })
+      .filter((s) => s.scheduledMinutes !== s.requiredMinutes); // keep incomplete or excess only
   },
 });
 
