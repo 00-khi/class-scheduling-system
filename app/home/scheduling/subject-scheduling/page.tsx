5a46d0b85b4adebab509f88c57595b4cb2ec8990
khiancarasicas <khianreendy.c@gmail.com>
2025-09-22 23:56:31 +0800

    added refresh trigger

--- app/home/scheduling/subject-scheduling/page.tsx
+++ app/home/scheduling/subject-scheduling/page.tsx
@@ -6,23 +6,38 @@ import { useState } from "react";
 import UnscheduledSubjectsManager from "./unscheduled-subject-manager";
 import { Card } from "@/ui/shadcn/card";
 import { ArrowRight, MousePointer2, Smile } from "lucide-react";
+import ScheduledSubjectManager from "./scheduled-subject-manager";
 
 export default function SubjectSchedulingPage() {
   const [selectedSectionId, setSelectedSectionId] = useState<number | null>(
     null
   );
 
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const triggerRefresh = () => setRefreshKey((prev) => prev + 1);
+
   return (
     <MainSection>
       <MainSection.Section>
-        <MainSection.Title>
-          Subject Scheduling {selectedSectionId}
-        </MainSection.Title>
+        <MainSection.Title>Subject Scheduling</MainSection.Title>
         <MainSection.Content>
           <div className="space-y-3">
             <SelectSectionCard onSectionChange={setSelectedSectionId} />
+
             {selectedSectionId ? (
-              <UnscheduledSubjectsManager sectionId={selectedSectionId} />
+              <div className="space-y-3">
+                <UnscheduledSubjectsManager
+                  sectionId={selectedSectionId}
+                  onChange={triggerRefresh}
+                  refreshKey={refreshKey}
+                />
+                <ScheduledSubjectManager
+                  sectionId={selectedSectionId}
+                  onChange={triggerRefresh}
+                  refreshKey={refreshKey}
+                />
+              </div>
             ) : (
               <Card>
                 <div className="flex flex-col justify-center items-center gap-2 text-muted-foreground">
