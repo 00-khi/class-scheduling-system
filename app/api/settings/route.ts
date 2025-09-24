0249e0200f4c2c874c9c79dc493452df2a178404
khiancarasicas <khianreendy.c@gmail.com>
2025-09-24 19:42:50 +0800

    edit: settings api - added validations

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { isValidDays, isValidTime } from "@/lib/schedule-utils";
+import { Day, Semester } from "@prisma/client";
+import { capitalizeEachWord } from "@/lib/utils";
 
 // GET all settings
 export async function GET() {
@@ -13,6 +16,41 @@ export async function POST(req: Request) {
 
   if (!key) {
     return NextResponse.json({ error: "Key required" }, { status: 400 });
+  }
+
+  if (!value) {
+    return NextResponse.json({ error: "Value required" }, { status: 400 });
+  }
+
+  const validSemesters = Object.values(Semester);
+
+  if ((key === "dayStart" || key === "dayEnd") && !isValidTime(value)) {
+    return NextResponse.json(
+      { error: "Time must be in 30 minute steps like 07:00 or 07:30" },
+      { status: 400 }
+    );
+  }
+
+  if (key === "semester" && !validSemesters.includes(value)) {
+    return NextResponse.json(
+      {
+        error: `Invalid semester value. It must be one of: ${validSemesters.join(
+          ", "
+        )}`,
+      },
+      { status: 400 }
+    );
+  }
+
+  if (key === "days" && !isValidDays(value)) {
+    return NextResponse.json(
+      {
+        error: `Days must be a JSON array of valid Day values: ${Object.values(
+          Day
+        ).join(", ")}`,
+      },
+      { status: 400 }
+    );
   }
 
   const setting = await prisma.setting.upsert({
