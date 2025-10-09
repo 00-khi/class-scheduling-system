4e341dc54849d11b16fb05cc0f487c4c4da6370d
khiancarasicas <khianreendy.c@gmail.com>
2025-10-09 15:06:40 +0800

    feat: implement GET handler to retrieve unassigned subjects and current semester from settings

---
+++ app/api/scheduled-subjects/unassigned/route.ts
@@ -0,0 +1,30 @@
+import { createApiHandler } from "@/lib/api/api-handler";
+import { prisma } from "@/lib/prisma";
+import { Semester } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export const GET = createApiHandler(async () => {
+  // get current semester from settings
+  const semesterSetting = await prisma.setting.findUnique({
+    where: { key: "semester" },
+  });
+  if (!semesterSetting) {
+    return NextResponse.json(
+      { error: "Semester setting not found" },
+      { status: 400 }
+    );
+  }
+  const currentSemester = semesterSetting.value as Semester;
+
+  const unassignedSubjects = await prisma.scheduledSubject.findMany({
+    where: {
+      scheduledInstructor: { none: {} },
+    },
+    include: {
+      room: true,
+      subject: true,
+    },
+  });
+
+  return NextResponse.json(unassignedSubjects);
+});
